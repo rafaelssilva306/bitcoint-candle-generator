02ae67e08e366e115cfccd11934d7b224f9e3a74
Rafael Silva <rafa.306@hotmail.com>
2023-06-22 12:40:38 -0300

    Criada a logica para trazer os candles atráves de periodos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { config } from 'dotenv'
 import axios from 'axios'
+import Period from './enums/Period'
+import Candle from './models/Candle'
 
 config()
 
@@ -11,4 +13,24 @@ const readMarketPrice = async (): Promise<number> => {
     return price
 }
 
-readMarketPrice()
+const generateCandle = async () => {
+
+    while(true) {
+        const loopTimes = Period.ONE_MINUTE / Period.TEN_SECONDS
+        const candle = new Candle('BTC')
+
+        console.log('--------------------------')
+        console.log('Gerando novo Candle')
+        for(let i = 0; i< loopTimes; i++) {
+            const price = await readMarketPrice()
+            candle.addValue(price)
+            console.log(`Preço de mercado #${i + 1} of ${loopTimes}`)
+            await new Promise(r => setTimeout(r, Period.TEN_SECONDS))
+        }
+        candle.closeCandle()
+        console.log('Candle encerrado')
+        console.log(candle.toSimpleObject())
+    }
+}
+
+generateCandle()
